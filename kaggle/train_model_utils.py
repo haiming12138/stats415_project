f5f91b9c741cbc2b8ba3fe5ff05f4f77e288f916
Haiming Li <102835461+haiming12138@users.noreply.github.com>
2023-11-14 16:06:43 -0500

    save_metric

--- kaggle/train_model_utils.py
+++ kaggle/train_model_utils.py
@@ -6,7 +6,8 @@ def save_cv_metric(classifier, X, y, name):
     metrics = ['r2', 'neg_mean_squared_error', 'neg_root_mean_squared_error',
                'neg_mean_absolute_percentage_error', 'explained_variance']
     res = cross_validate(classifier, X, y, cv=KFold(n_splits=10, shuffle=True, 
-                                                    random_state=614), scoring=metrics)
+                                                    random_state=614), 
+                                                    scoring=metrics, n_jobs=-1)
 
     output = ''
     for metric in metrics:
